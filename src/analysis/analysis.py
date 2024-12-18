c923451d06ee17c37a63930abfeba7d36b19e49f
HieuTrungCao <21021490@vnu.edu.vn>
2024-12-18 14:45:46 +0700

    Add kien data

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-data = pd.read_csv("data/GereralAI/train/question.csv")
+data = pd.read_csv("data/GereralAI/train/train.csv")
 
 title = 'Distribution of question + llm_answer Lengths'
 data['question_llm_answer_length'] = data['question'].apply(len) + data['llm_answer'].apply(len)
